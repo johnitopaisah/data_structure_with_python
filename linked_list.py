66d20889c8da2308c06a6bdcf79e0b62dc6ce95a
johnitopaisah <johnitopaisah@gmail.com>
2024-06-28 02:56:49 +0200

    Add linked_list file

---
+++ linked_list.py
@@ -0,0 +1,41 @@
+class node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class linked_list:
+    def __init__(self):
+        self.head = node()
+    
+    def append(self, data):
+        new_node = node(data)
+        curr = self.head
+        while curr.next != None:
+            curr = curr.next
+        curr.next = new_node
+    
+    def length(self):
+        curr = self.head
+        total = 0
+        while curr.next != None:
+            total += 1
+            curr = curr.next
+        return total
+    
+    def display(self):
+        elems = []
+        curr_node = self.head
+        while curr_node.next != None:
+            curr_node = curr_node.next
+            elems.append(curr_node.data)
+        print(elems)
+
+
+my_list = linked_list()
+my_list.display()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+my_list.display()
