f214af70a8c191d36c40bae2d8f4d2b3e6abdbcf
johnitopaisah <johnitopaisah@gmail.com>
2024-06-28 22:06:43 +0200

    building triangle with *

--- triangle1.py
+++ triangle1.py
@@ -2,16 +2,6 @@ height = int(input("Enter the heigth of the triangle: "))
 
 for row in range(1, height+1):
     if row <= (height//2):
-        print('* '*(row), end=' ')
+        print('* '*(row))
     else:
-        print('* '*(height-row+1), end=' ')
-    print()
-
-# height = int(input("Enter the heigth of the triangle: "))
-
-# for row in range(1, height+1):
-#     if row <= (height//2):
-#         print('* '*(row), end=' ')
-#     else:
-#         print(' '*(height-row+1), end=' ')
-#     print()
+        print('* '*(height-row+1))
